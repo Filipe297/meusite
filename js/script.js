2a1fe7807e4c04805e953eb15a1fc8b5b5430a38
FilipeRodrigues <filipermb@users.noreply.github.com>
2021-12-14 02:52:56 -0300

    Deixando responsivo
    Adaptando o site para todo tipo de tela.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(function(){
     var spantrue = $('a[noart="true"] span');
     var spanfalse = $('a[noart="false"] span');
     var divArt1 = $('.aviso');
+    var contaviso = $('.contaviso');
 
     var id1 = $('#id1');
     var id2 = $('#id2');
@@ -13,12 +14,17 @@ $(function(){
     spanfalse.html("<span>Artigo</span>")
     
     divAtrue.click(function(e){
-       divArt1.css("left","-300px")
+        contaviso.css("display","inline-block");
+        divArt1.css("left","-400px");
         setTimeout(function() {
-            divArt1.css("left","0px")
+            divArt1.css("left","0px");
         }, 100);
         setTimeout(function() {
-            divArt1.css("left","-300px")
+            divArt1.css("left","-400px");
+            setTimeout(function() {
+                contaviso.css("display","none");
+            }, 1000);
         }, 2000);
+        
     });
 });
