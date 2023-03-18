24d81d709cd538e85089313a59c2a22cf46c5074
FilipeRodrigues <filipermb@users.noreply.github.com>
2023-03-18 13:52:17 -0300

    UPDATE

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function(){
 
+    alert('opa');
+
     var divAtrue = $('a[noart="true"] ');
     var divAfalse = $('a[noart="false"]');
     var spantrue = $('a[noart="true"] span');
@@ -10,18 +12,25 @@ $(function(){
     var id1 = $('#id1');
     var id2 = $('#id2');
 
+    var time;
+    var time1;
+    var time2;
+
     spantrue.html("<span>Artigo em produção</span>")
     spanfalse.html("<span>Artigo</span>")
     
     divAtrue.click(function(e){
+        clearTimeout(time);
+        clearTimeout(time1);
+        clearTimeout(time2);
         contaviso.css("display","inline-block");
         divArt1.css("left","-400px");
-        setTimeout(function() {
+        time = setTimeout(function() {
             divArt1.css("left","0px");
         }, 100);
-        setTimeout(function() {
+        time1 = setTimeout(function() {
             divArt1.css("left","-400px");
-            setTimeout(function() {
+            time2 = setTimeout(function() {
                 contaviso.css("display","none");
             }, 1000);
         }, 2000); 
---
+++ js/teste.js
@@ -0,0 +1,1 @@
+alert('PROVA DE CONCEITO');
